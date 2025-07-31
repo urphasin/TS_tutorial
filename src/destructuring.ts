3ec3314040a09018f8fb105543437381943d8b2a
amuga.otito(computer dorm) <urphasin@gmail.com>
2025-07-30 23:24:19 -0600

    first commit hallelujah

---
+++ src/destructuring.ts
@@ -0,0 +1,13 @@
+const numbers = [10, 20, 30];
+const strings = ["10", "20", "30"];
+
+const[a, b, c] = numbers;
+const[d, e, f] = strings;
+
+console.log(a); // 10
+console.log(b); // 20
+console.log(c); // 30
+
+console.log(d);
+console.log(e);
+console.log(f);
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+console.log('Happy developing ✨')
+
+var z = [1, 2, 3];
+
+let j = z.map(function(e, i) {
+    console.log("Value = " + e);
+    console.log("Index = " + i)
+    return String(e)
+})
+
+// console.log(a);
